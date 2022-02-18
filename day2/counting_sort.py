27d0fb5cb7ff8ec980c3901617b516f24a0eb5c3
daplho <nskirov@gmail.com>
2022-02-18 11:21:41 -0800

    * solved counting sort

---
+++ day2/counting_sort.py
@@ -0,0 +1,27 @@
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'countingSort' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+def countingSort(arr):
+    frequencyArr = [0] * 100
+    for i in arr:
+        frequencyArr[i] += 1
+
+    return frequencyArr
+
+if __name__ == '__main__':
+    n = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = countingSort(arr)
+    print(result)
